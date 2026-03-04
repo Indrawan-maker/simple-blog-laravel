846afe070c837cde544c57a37cdbfd438e995a7c
Indrawan-maker <indrawanismail0@gmail.com>
2026-03-04 20:07:39 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('/authors/{user:name}', function(User $user) {
 
 Route::get('/kategori/{kategori:slug}', function(Kategori $kategori) {
     return view('berita', [
-        'title' => 'Kategori berita' . $kategori->name,
+        'title' => 'Kategori berita ' . $kategori->name,
         'mainposts' => $kategori->posts,
         'secondposts' => collect()
     ]);
